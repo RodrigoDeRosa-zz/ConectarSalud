4c7b2957be3d751f3b90e2a5ee871bc1a5b95f41
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2020-06-14 13:40:36 -0300

    Added bottom nav with activities -> Refactor to fragment

--- app/src/main/java/com/conectarSalud/home/affiliate/HomeAffiliateActivity.kt
+++ app/src/main/java/com/conectarSalud/home/affiliate/HomeAffiliateActivity.kt
@@ -6,10 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.conectarSalud.R
 import com.conectarSalud.videoChat.VideoChatViewActivity
 import com.conectarSalud.videoChat.WaitingRoomActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home_affiliate.*
 
 class HomeAffiliateActivity : AppCompatActivity() {
-
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_affiliate)
@@ -18,6 +19,25 @@ class HomeAffiliateActivity : AppCompatActivity() {
             finish()
             val intent = Intent(this, WaitingRoomActivity::class.java)
             startActivity(intent)
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.page_home -> {
+                    val intent = Intent(this, HomeAffiliateActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.page_history -> {
+                    finish()
+                    val intent = Intent(this, HomeAffiliateActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
 
     }
