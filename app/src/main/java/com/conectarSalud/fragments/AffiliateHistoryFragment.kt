708689305048236a6da447a0b09dbdab3ef7cca9
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2020-06-15 18:29:29 -0300

    Added Recycler View to patient history

--- app/src/main/java/com/conectarSalud/fragments/AffiliateHistoryFragment.kt
+++ app/src/main/java/com/conectarSalud/fragments/AffiliateHistoryFragment.kt
@@ -1,12 +1,15 @@
 package com.conectarSalud.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.os.Bundle
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.conectarSalud.R
+import com.conectarSalud.adapter.HistoryRVAdapter
+import com.conectarSalud.model.HistoryAffiliateItemModel
 
 class AffiliateHistoryFragment: Fragment() {
 
@@ -18,7 +21,29 @@ class AffiliateHistoryFragment: Fragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_affiliate_history, container, false)
+        val view: View = inflater.inflate(
+            com.conectarSalud.R.layout.fragment_affiliate_history,
+            container, false
+        )
+        var historyRv: RecyclerView = view.findViewById(R.id.history_affiliate_recycler_view) as RecyclerView
+        var mLayoutManager = LinearLayoutManager(this.activity)
+        historyRv.layoutManager = mLayoutManager
+
+        val adapter = context?.let { HistoryRVAdapter(getHistory(), it) }
+        historyRv.adapter = adapter
+
+        return view
+    }
+
+    private fun getHistory(): ArrayList<HistoryAffiliateItemModel> {
+        val histories: ArrayList<HistoryAffiliateItemModel> = ArrayList()
+        histories.add(HistoryAffiliateItemModel("Jorge","García", listOf("Medico","Ginecologo"),"01-01-2020"))
+        histories.add(HistoryAffiliateItemModel("Pedro","Marmol", listOf("Pediatra"),"01-12-2019"))
+        histories.add(HistoryAffiliateItemModel("Jorge","García", listOf("Medico","Ginecologo"),"01-01-2020"))
+        histories.add(HistoryAffiliateItemModel("Pedro","Marmol", listOf("Pediatra"),"01-12-2019"))
+        histories.add(HistoryAffiliateItemModel("Jorge","García", listOf("Medico","Ginecologo"),"01-01-2020"))
+        histories.add(HistoryAffiliateItemModel("Pedro","Marmol", listOf("Pediatra"),"01-12-2019"))
+        return histories
     }
 
 }
